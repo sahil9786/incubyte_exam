30ebdb33b1f7a3bb770dcdc08a3d6da777f93684
unknown <patelsahil292@gmail.com>
2021-10-18 00:13:56 +0530

    test3

--- String_Calculator/src/Calculator/StringCalculatorTest.java
+++ String_Calculator/src/Calculator/StringCalculatorTest.java
@@ -18,4 +18,9 @@ public class StringCalculatorTest {
 	public void testOneNumber() {
 		assertEquals(1, Calculator.add("1"));
 	}
+	@Test
+	public void testTwoNumbers(){
+		assertEquals(3, Calculator.add("1,2"));
+	}
+	
 }
